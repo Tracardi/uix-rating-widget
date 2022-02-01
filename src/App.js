96b30b70ba8d396bce7d232480a4df3e0ab2c7c1
DawidekZagajnik <krukdawid27@gmail.com>
2022-02-01 16:40:55 +0100

    commit 2

--- src/App.js
+++ src/App.js
@@ -99,13 +99,13 @@ function App({ domElement }) {
                             <Typography variant="h5" color="inherit">{title}</Typography>
                         </Box>
                         <Box sx={{ color: darkTheme ? "#ffffff" : "#343434" }}>
-                            <p>{message}</p>
+                            <p style={{ marginTop: 1, marginBottom: 5 }}>{message}</p>
                         </Box>
                         <Box>
                             <span onClick={() => sendRating(1)} onMouseOver={() => { setStars(1); }} onMouseLeave={() => { setStars(0); }}>
                                 <AiFillStar
                                     style={{
-                                        color: selectedStars > 0 ? "yellow" : "grey",
+                                        color: selectedStars > 0 ? "#ffd700" : "grey",
                                         margin: selectedStars > 0 ? 0: 2
                                     }}
                                     size={selectedStars > 0 ? 40 : 36} />
@@ -113,7 +113,7 @@ function App({ domElement }) {
                             <span onClick={() => sendRating(2)} onMouseOver={() => { setStars(2); }} onMouseLeave={() => { setStars(0); }}>
                                 <AiFillStar
                                     style={{
-                                        color: selectedStars > 1 ? "yellow" : "grey",
+                                        color: selectedStars > 1 ? "#ffd700" : "grey",
                                         margin: selectedStars > 1 ? 0 : 2
                                     }}
                                     size={selectedStars > 1 ? 40 : 36} />
@@ -121,7 +121,7 @@ function App({ domElement }) {
                             <span onClick={() => sendRating(3)} onMouseOver={() => { setStars(3); }} onMouseLeave={() => { setStars(0); }}>
                                 <AiFillStar
                                     style={{
-                                        color: selectedStars > 2 ? "yellow" : "grey",
+                                        color: selectedStars > 2 ? "#ffd700" : "grey",
                                         margin: selectedStars > 2 ? 0 : 2
                                     }}
                                     size={selectedStars > 2 ? 40 : 36} />
@@ -129,7 +129,7 @@ function App({ domElement }) {
                             <span onClick={() => sendRating(4)} onMouseOver={() => { setStars(4); }} onMouseLeave={() => { setStars(0); }}>
                                 <AiFillStar
                                     style={{
-                                        color: selectedStars > 3 ? "yellow" : "grey",
+                                        color: selectedStars > 3 ? "#ffd700" : "grey",
                                         margin: selectedStars > 3 ? 0 : 2
                                     }}
                                     size={selectedStars > 3 ? 40 : 36} />
@@ -137,7 +137,7 @@ function App({ domElement }) {
                             <span onClick={() => sendRating(5)} onMouseOver={() => { setStars(5); }} onMouseLeave={() => { setStars(0); }}>
                                 <AiFillStar
                                     style={{
-                                        color: selectedStars > 4 ? "yellow" : "grey",
+                                        color: selectedStars > 4 ? "#ffd700" : "grey",
                                         margin: selectedStars > 4 ? 0 : 2
                                     }}
                                     size={selectedStars > 4 ? 40 : 36} />
