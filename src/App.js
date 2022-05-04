8d1c0295f9a5268ec8c8b536be7ec20803144508
DawidekZagajnik <krukdawid27@gmail.com>
2022-05-04 16:42:28 +0200

    style fix

--- src/App.js
+++ src/App.js
@@ -74,79 +74,85 @@ function App({ domElement }) {
 
 
     return (
+        <>
+        <style>{`
+            .RatingSnackbar .RatingSnackbarContent {
+                background-color: ${darkTheme ? "#343434" : "#ffffff"};
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+            }
+            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
+            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .RatingSnackbarTitle {
+                color: ${darkTheme ? "#ffffff" : "#343434"};
+            }
+            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .RatingSnackbarMessageBox {
+                color: ${darkTheme ? "#ffffff" : "#343434"};
+            }
+            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star0 {
+                color: ${selectedStars > 0 ? "#ffd700" : "grey"};
+                margin: ${selectedStars > 0 ? "0px": "2px"};
+            }
+            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star1 {
+                color: ${selectedStars > 1 ? "#ffd700" : "grey"};
+                margin: ${selectedStars > 1 ? "0px": "2px"};
+            }
+            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star2 {
+                color: ${selectedStars > 2 ? "#ffd700" : "grey"};
+                margin: ${selectedStars > 2 ? "0px": "2px"};
+            }
+            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star3 {
+                color: ${selectedStars > 3 ? "#ffd700" : "grey"};
+                margin: ${selectedStars > 3 ? "0px": "2px"};
+            }
+            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star4 {
+                color: ${selectedStars > 4 ? "#ffd700" : "grey"};
+                margin: ${selectedStars > 4 ? "0px": "2px"};
+            }
+        `}</style>
         <Snackbar
+            className="RatingSnackbar"
             open={open}
             anchorOrigin={{ vertical: verticalPosition, horizontal: horizontalPosition }}
             onClose={handleClose}
             autoHideDuration={autoHide}
         >
             <SnackbarContent
-                style={{
-                    backgroundColor: darkTheme ? "#343434" : "#ffffff",
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "center"
-                }}
+                className="RatingSnackbarContent"
                 message={
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center"
-                        }}
-                    >
-                        <Box sx={{ color: darkTheme ? "#ffffff" : "#343434" }}>
+                    <Box className="RatingSnackbarMessage">
+                        <Box className="RatingSnackbarTitle">
                             <Typography variant="h5" color="inherit">{title}</Typography>
                         </Box>
-                        <Box sx={{ color: darkTheme ? "#ffffff" : "#343434" }}>
+                        <Box className="RatingSnackbarMessageBox">
                             <p style={{ marginTop: 1, marginBottom: 5 }}>{message}</p>
                         </Box>
-                        <Box>
+                        <Box className="Stars">
                             <span onClick={() => sendRating(1)} onMouseOver={() => { setStars(1); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar
-                                    style={{
-                                        color: selectedStars > 0 ? "#ffd700" : "grey",
-                                        margin: selectedStars > 0 ? 0: 2
-                                    }}
-                                    size={selectedStars > 0 ? 40 : 36} />
+                                <AiFillStar className="Star0" size={selectedStars > 0 ? 40 : 36} />
                             </span>
                             <span onClick={() => sendRating(2)} onMouseOver={() => { setStars(2); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar
-                                    style={{
-                                        color: selectedStars > 1 ? "#ffd700" : "grey",
-                                        margin: selectedStars > 1 ? 0 : 2
-                                    }}
-                                    size={selectedStars > 1 ? 40 : 36} />
+                                <AiFillStar className="Star1" size={selectedStars > 1 ? 40 : 36} />
                             </span>
                             <span onClick={() => sendRating(3)} onMouseOver={() => { setStars(3); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar
-                                    style={{
-                                        color: selectedStars > 2 ? "#ffd700" : "grey",
-                                        margin: selectedStars > 2 ? 0 : 2
-                                    }}
-                                    size={selectedStars > 2 ? 40 : 36} />
+                                <AiFillStar className="Star2" size={selectedStars > 2 ? 40 : 36} />
                             </span>
                             <span onClick={() => sendRating(4)} onMouseOver={() => { setStars(4); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar
-                                    style={{
-                                        color: selectedStars > 3 ? "#ffd700" : "grey",
-                                        margin: selectedStars > 3 ? 0 : 2
-                                    }}
-                                    size={selectedStars > 3 ? 40 : 36} />
+                                <AiFillStar className="Star3" size={selectedStars > 3 ? 40 : 36} />
                             </span>
                             <span onClick={() => sendRating(5)} onMouseOver={() => { setStars(5); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar
-                                    style={{
-                                        color: selectedStars > 4 ? "#ffd700" : "grey",
-                                        margin: selectedStars > 4 ? 0 : 2
-                                    }}
-                                    size={selectedStars > 4 ? 40 : 36} />
+                                <AiFillStar className="Star4" size={selectedStars > 4 ? 40 : 36} />
                             </span>
                         </Box>
                     </Box>
                 }
             />
         </Snackbar>
+        </>
     );
 
 }
