58e99cb74aa20f39bb9707f91cedc3bc37950eee
risto <risto.kowaczewski@gmail.com>
2022-09-18 00:00:38 +0200

    Styling the widget

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import "regenerator-runtime/runtime";
-import { Box, Snackbar, SnackbarContent, Typography } from "@material-ui/core";
-import { AiFillStar } from "react-icons/ai";
+import {Box, Snackbar, SnackbarContent} from "@material-ui/core";
+import {AiFillStar} from "react-icons/ai";
+import Fade from "@material-ui/core/Fade";
 
 
-function App({ domElement }) {
+function App({domElement}) {
 
     const [open, setOpen] = React.useState(true);
     const [selectedStars, setStars] = React.useState(0);
 
     const verticalPosition = domElement.getAttribute("data-position-vertical");
     const horizontalPosition = domElement.getAttribute("data-position-horizontal");
-    const title = domElement.getAttribute("data-title");
-    const message = domElement.getAttribute("data-message");
-    const eventType = domElement.getAttribute("data-event-type");
+    const title = domElement.getAttribute("data-title") || "title";
+    const message = domElement.getAttribute("data-message") || "message";
+    const eventType = domElement.getAttribute("data-event-type") || "rating";
     const apiUrl = domElement.getAttribute("data-api-url");
-    const darkTheme = domElement.getAttribute("data-theme") === "dark";
     const autoHide = parseInt(domElement.getAttribute("data-auto-hide"), 10) * 1000;
     const sourceId = domElement.getAttribute("data-source-id");
     const sessionId = domElement.getAttribute("data-session-id");
     const profileId = domElement.getAttribute("data-profile-id");
     const saveEvent = domElement.getAttribute("data-save-event") === "yes";
-
+    const titleSize = domElement.getAttribute("data-title-size") || "22px";
+    const descSize = domElement.getAttribute("data-desc-size") || "15px";
+    const starSize = domElement.getAttribute("data-star-size") || "24px";
+    const selectedStarColor = domElement.getAttribute("data-selected-star-color") || "#ffd700"
+    const unselectedStarColor = domElement.getAttribute("data-unselected-star-color") || "#ccc"
+    const boxStyle = domElement.getAttribute("data-box-style") || "elevation"
+    const boxPadding = domElement.getAttribute("data-box-padding") || "10px"
+    const boxElevation = domElement.getAttribute("data-box-elevation") || "1"
+    const boxBackgroundColor = domElement.getAttribute("data-box-gb-color") || "white"
+    const textColor = domElement.getAttribute("data-box-text-color") || "black"
 
     const handleClose = (event, reason) => {
         if (reason === "clickaway") {
@@ -33,125 +42,143 @@ function App({ domElement }) {
     }
 
     const sendRating = async (rating) => {
-
-        const response = await fetch(`${apiUrl}/track`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "source": {
-                    "id": sourceId
+        try {
+            await fetch(`${apiUrl}/track`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
                 },
-                "session": {
-                    "id": sessionId
-                },
-                "profile": {
-                    "id": profileId
-                },
-                "context": {},
-                "properties": {},
-                "events": [
-                    {
-                        "type": eventType,
-                        "properties": {
-                            "rating": rating
-                        },
-                        "options": {
-                            "saveEvent": saveEvent
+                body: JSON.stringify({
+                    "source": {
+                        "id": sourceId
+                    },
+                    "session": {
+                        "id": sessionId
+                    },
+                    "profile": {
+                        "id": profileId
+                    },
+                    "context": {},
+                    "properties": {},
+                    "events": [
+                        {
+                            "type": eventType,
+                            "properties": {
+                                "rating": rating
+                            },
+                            "options": {
+                                "saveEvent": saveEvent
+                            }
                         }
-                    }
-                ],
-                "options": {}
+                    ],
+                    "options": {}
+                })
             })
-        }).catch(error => console.log(error))
-
-        const data = await response.json();
-
-        setOpen(false);
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setOpen(false);
+        }
     }
 
-
     return (
-        <>
-        <style>{`
-            .RatingSnackbar .RatingSnackbarContent {
-                background-color: ${darkTheme ? "#343434" : "#ffffff"};
-                display: flex;
-                flex-direction: row;
-                justify-content: center;
-            }
-            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-            }
-            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .RatingSnackbarTitle {
-                color: ${darkTheme ? "#ffffff" : "#343434"};
-            }
-            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .RatingSnackbarMessageBox {
-                color: ${darkTheme ? "#ffffff" : "#343434"};
-            }
-            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star0 {
-                color: ${selectedStars > 0 ? "#ffd700" : "grey"};
-                margin: ${selectedStars > 0 ? "0px": "2px"};
-            }
-            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star1 {
-                color: ${selectedStars > 1 ? "#ffd700" : "grey"};
-                margin: ${selectedStars > 1 ? "0px": "2px"};
-            }
-            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star2 {
-                color: ${selectedStars > 2 ? "#ffd700" : "grey"};
-                margin: ${selectedStars > 2 ? "0px": "2px"};
-            }
-            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star3 {
-                color: ${selectedStars > 3 ? "#ffd700" : "grey"};
-                margin: ${selectedStars > 3 ? "0px": "2px"};
-            }
-            .RatingSnackbar .RatingSnackbarContent .RatingSnackbarMessage .Stars .Star4 {
-                color: ${selectedStars > 4 ? "#ffd700" : "grey"};
-                margin: ${selectedStars > 4 ? "0px": "2px"};
-            }
-        `}</style>
+
         <Snackbar
-            className="RatingSnackbar"
             open={open}
-            anchorOrigin={{ vertical: verticalPosition, horizontal: horizontalPosition }}
+            anchorOrigin={{vertical: verticalPosition, horizontal: horizontalPosition}}
             onClose={handleClose}
             autoHideDuration={autoHide}
         >
             <SnackbarContent
-                className="RatingSnackbarContent"
+                style={
+                    {
+                        backgroundColor: boxBackgroundColor,
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        padding: boxPadding
+                    }
+                }
+                elevation={boxElevation}
+                variant={boxStyle}
                 message={
-                    <Box className="RatingSnackbarMessage">
-                        <Box className="RatingSnackbarTitle">
-                            <Typography variant="h5" color="inherit">{title}</Typography>
+                    <Fade in={open} {...(open ? {timeout: 2000} : {})}>
+                        <Box style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            color: textColor
+                        }}>
+                            {title && <div style={{fontSize: titleSize}}>{title}</div>}
+                            {message && <p style={{marginTop: 1, marginBottom: 5, fontSize: descSize}}>{message}</p>}
+                            <div style={{cursor: "pointer", marginTop: 10}}>
+                            <span onClick={() => sendRating(1)} onMouseOver={() => {
+                                setStars(1);
+                            }} onMouseLeave={() => {
+                                setStars(0);
+                            }}>
+                                    <AiFillStar style={
+                                        {
+                                            color: selectedStars > 0 ? selectedStarColor : "grey",
+                                            margin: 2
+                                        }
+                                    } size={starSize}/>
+                            </span>
+                                <span onClick={() => sendRating(2)} onMouseOver={() => {
+                                    setStars(2);
+                                }} onMouseLeave={() => {
+                                    setStars(0);
+                                }}>
+                                    <AiFillStar style={
+                                        {
+                                            color: selectedStars > 1 ? selectedStarColor : unselectedStarColor,
+                                            margin: 2
+                                        }
+                                    } size={starSize}/>
+                                </span>
+                                <span onClick={() => sendRating(3)} onMouseOver={() => {
+                                    setStars(3);
+                                }} onMouseLeave={() => {
+                                    setStars(0);
+                                }}>
+                                    <AiFillStar style={
+                                        {
+                                            color: selectedStars > 2 ? selectedStarColor : unselectedStarColor,
+                                            margin: 2
+                                        }
+                                    } size={starSize}/>
+                                </span>
+                                <span onClick={() => sendRating(4)} onMouseOver={() => {
+                                    setStars(4);
+                                }} onMouseLeave={() => {
+                                    setStars(0);
+                                }}>
+                                    <AiFillStar style={
+                                        {
+                                            color: selectedStars > 3 ? selectedStarColor : unselectedStarColor,
+                                            margin: 2
+                                        }
+                                    } size={starSize}/>
+                                </span>
+                                <span onClick={() => sendRating(5)} onMouseOver={() => {
+                                    setStars(5);
+                                }} onMouseLeave={() => {
+                                    setStars(0);
+                                }}>
+                                    <AiFillStar style={
+                                        {
+                                            color: selectedStars > 4 ? selectedStarColor : unselectedStarColor,
+                                            margin: 2
+                                        }
+                                    } size={starSize}/>
+                                </span>
+                            </div>
                         </Box>
-                        <Box className="RatingSnackbarMessageBox">
-                            <p style={{ marginTop: 1, marginBottom: 5 }}>{message}</p>
-                        </Box>
-                        <Box className="Stars">
-                            <span onClick={() => sendRating(1)} onMouseOver={() => { setStars(1); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar className="Star0" size={selectedStars > 0 ? 40 : 36} />
-                            </span>
-                            <span onClick={() => sendRating(2)} onMouseOver={() => { setStars(2); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar className="Star1" size={selectedStars > 1 ? 40 : 36} />
-                            </span>
-                            <span onClick={() => sendRating(3)} onMouseOver={() => { setStars(3); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar className="Star2" size={selectedStars > 2 ? 40 : 36} />
-                            </span>
-                            <span onClick={() => sendRating(4)} onMouseOver={() => { setStars(4); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar className="Star3" size={selectedStars > 3 ? 40 : 36} />
-                            </span>
-                            <span onClick={() => sendRating(5)} onMouseOver={() => { setStars(5); }} onMouseLeave={() => { setStars(0); }}>
-                                <AiFillStar className="Star4" size={selectedStars > 4 ? 40 : 36} />
-                            </span>
-                        </Box>
-                    </Box>
+                    </Fade>
                 }
             />
         </Snackbar>
-        </>
+
     );
 
 }
